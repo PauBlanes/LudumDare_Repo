e2c9affceabc3f821b05ba7218ccf0d5436b41a4
Pau Blanes <paublanespardo@enti.cat>
2018-12-01 12:00:27 +0100

    Added project

---
+++ LD_TowerDefense/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Move();
+        
+    }
+
+    void Move()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {            
+            transform.rotation = transform.rotation = Quaternion.Euler(0, 0, 180);
+            transform.position += Vector3.up * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.rotation = transform.rotation = Quaternion.Euler(0, 0, 0);
+            transform.position += Vector3.down * speed * Time.deltaTime;
+
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.rotation = transform.rotation = Quaternion.Euler(0, 0, 270);
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.rotation = transform.rotation = Quaternion.Euler(0, 0, 90);
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+
+        
+    }
+}
