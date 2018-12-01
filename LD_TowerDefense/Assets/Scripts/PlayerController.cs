74eaf011fd567918efec6eb5aa2cf6b880821e7a
Pau Blanes <paublanespardo@enti.cat>
2018-12-01 13:16:32 +0100

    movimiento y disparo

--- LD_TowerDefense/Assets/Scripts/PlayerController.cs
+++ LD_TowerDefense/Assets/Scripts/PlayerController.cs
@@ -4,44 +4,95 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-    public float speed;
+    //Shooting
+    [System.Serializable]
+    public struct Weapon
+    {
+        public float fireRate;        
+        public GameObject bullet;        
+    }
+    private Weapon equipedWeapon;
+    private float nextFire;
+    public Weapon[] weapons;
+    public Transform fireSpawn;
 
-	// Use this for initialization
-	void Start () {
-		
+    //movement
+    public float speed;
+    private bool moving;
+
+
+    // Use this for initialization
+    void Start () {
+        equipedWeapon = weapons[0];	  
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
         Move();
-        
+
+        if (Input.GetKey(KeyCode.Space) && Time.time > nextFire)
+        {
+            nextFire = Time.time + equipedWeapon.fireRate;
+            Shoot();
+        }
     }
 
     void Move()
     {
-        if (Input.GetKey(KeyCode.W))
-        {            
-            transform.rotation = transform.rotation = Quaternion.Euler(0, 0, 180);
-            transform.position += Vector3.up * speed * Time.deltaTime;
+        //Rotations
+        if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.A))
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 225);
+            moving = true;
         }
-        if (Input.GetKey(KeyCode.S))
+        else if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.D))
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 135);
+            moving = true;
+        }
+        else if (Input.GetKey(KeyCode.W))
+        {
+            transform.rotation = transform.rotation = Quaternion.Euler(0, 0, 180);
+            moving = true;
+        }
+        else if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.A))
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 315);
+            moving = true;
+        }
+        else if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.D))
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 45);
+            moving = true;
+        }
+        else if (Input.GetKey(KeyCode.S))
         {
             transform.rotation = transform.rotation = Quaternion.Euler(0, 0, 0);
-            transform.position += Vector3.down * speed * Time.deltaTime;
-
+            moving = true;
         }
-        if (Input.GetKey(KeyCode.A))
+        else if (Input.GetKey(KeyCode.A))
         {
             transform.rotation = transform.rotation = Quaternion.Euler(0, 0, 270);
-            transform.position += Vector3.left * speed * Time.deltaTime;
+            moving = true;
         }
-        if (Input.GetKey(KeyCode.D))
+        else if (Input.GetKey(KeyCode.D))
         {
             transform.rotation = transform.rotation = Quaternion.Euler(0, 0, 90);
-            transform.position += Vector3.right * speed * Time.deltaTime;
+            moving = true;
         }
+        else
+            moving = false;
 
-        
+        if (moving)
+            transform.position += -transform.up * speed * Time.deltaTime;
+
+    }
+
+    public void Shoot()
+    {        
+        GameObject b = Instantiate(equipedWeapon.bullet, fireSpawn.position, transform.rotation);
+        b.GetComponent<bullet>().direction -= transform.up;
+
     }
 }
---
+++ LD_TowerDefense/Assets/Scripts/bullet.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour {
+
+    public float speed;
+    public float damage;
+
+    public Vector3 direction;
+
+    private float lifeCounter;
+    private float lifeTime = 2;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position += direction * Time.deltaTime * speed;
+
+        lifeCounter += Time.deltaTime;
+        if (lifeCounter >= lifeTime)
+            Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag != "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
